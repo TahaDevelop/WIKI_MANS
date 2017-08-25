6b42eac5867a745f2d0c27b885760ce7a70f8b64
sypetitplaisir <sy.petitplaisir@gmail.com>
2017-08-25 09:51:58 +0200

    fichier de test

--- src/main/java/com/formation/test/UtilisateurDAOtest.java
+++ src/main/java/com/formation/test/UtilisateurDAOtest.java
@@ -9,20 +9,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.formation.wiki.dao.UtilisateurDAO;
+import com.formation.wiki.entity.Utilisateur;
 
 public class UtilisateurDAOtest {
 	
+	private Utilisateur testU;
+	
+	@Before
+	public void executeBeforeTest() {
+		 testU = new Utilisateur();
+	}
 	
 	@Test
 	public static void isUserExist() throws TimeoutException,SQLException {
-		
-		
-		
-		
-		
-		
-		
+	}
+	
+	@Test
+	public void AuthentificationUsertest() {
 		
 	}
-
+	
+	@Test
+	public void CreationUsertest() {
+		assertNull(testU);
+	}
 }
+
+
+
