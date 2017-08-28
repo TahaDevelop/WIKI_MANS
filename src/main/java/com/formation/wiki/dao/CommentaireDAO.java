2ef691d2907ca5542d669cee03fd6b1e69496519
cateve72 <31311611+cateve72@users.noreply.github.com>
2017-08-28 15:14:35 +0200

    Update CommentaireDAO.java

--- src/main/java/com/formation/wiki/dao/CommentaireDAO.java
+++ src/main/java/com/formation/wiki/dao/CommentaireDAO.java
@@ -1,10 +1,6 @@
 package com.formation.wiki.dao;
 
-<<<<<<< HEAD
-public class CommentaireDAO {
 
-}
-=======
 import java.sql.SQLException;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
@@ -79,9 +75,3 @@ public class CommentaireDAO {
 }
 
 
-
-
-
-
-
->>>>>>> 806791240ec046dbb4e6819fc6c0ffbbcda06d40
