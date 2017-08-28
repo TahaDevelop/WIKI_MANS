deecc143bd3883b514a2f27492b753a283635d7e
unknown <sahobindrainibe@gmail.com>
2017-08-28 15:28:38 +0200

    - AJout des tests :
    	* getbyLogin
    	* getByID

--- src/main/java/com/formation/test/UtilisateurDAOtest.java
+++ src/main/java/com/formation/test/UtilisateurDAOtest.java
@@ -2,51 +2,59 @@ package com.formation.test;
 
 import static org.junit.Assert.*;
 
-import java.sql.SQLException;
-import java.util.concurrent.TimeoutException;
+import javax.persistence.NoResultException;
+
+import org.junit.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.formation.wiki.dao.UtilisateurDAO;
 import com.formation.wiki.entity.Role;
 import com.formation.wiki.entity.Utilisateur;
+import com.formation.wiki.dao.*;
 
-public class UtilisateurDAOtest {
-	
+import junit.framework.TestCase;
 
+ 
+public class UtilisateurDAOtest {	
 	private Utilisateur testU;
+	private UtilisateurDAO utilisateurDAOtest;
 	Role testRole;
 	String role_name;
+	Utilisateur user;
 	
 	@Before
 	public void executeBeforeTest() {
 		 role_name=null;
 		 testU = new Utilisateur();
 		 testRole = new Role();
+		 utilisateurDAOtest=new UtilisateurDAO();
 		 testRole.setName("ADMIN");
-		 testU.setRole(testRole);
+		 testU.setRole(testRole);	 
+		 
+		 testU.setLogin("Sahobi");
+ 		 testU.setIdUser(1);
+		 
 	}
 	
-	@Test
-	public static void isUserExist() throws TimeoutException,SQLException {
+ 
+	/**Autheur  : Sahobi
+	 * Objectif : verifier l'utilisateur à partir du Login="Sahobi"
+	 * */
+ 	@Test(expected=NoResultException.class)
+	public void getUserByLoginTest(){		
+ 		assertNotNull(utilisateurDAOtest.getUserByLogin("Sahobi"));
+ 		assertNull(utilisateurDAOtest.getUserByLogin("NON"));
+ 		
 	}
-	
-	@Test
-	public void authentificationUsertest() {
-		assertEquals(testU.getRole().getName(),"ADMIN");
-	}
-	
-	@Test
-	public void creationUsertest() {
-		assertNull(testU);
-	}
-	
-	@Test
-	public void activerUsertest() {
-		
-	}
-	
+ 	
+ 	/**Autheur  : Sahobi
+	 * Objectif : verifier l'utilisateur à partir d'un IdUser
+	 * */
+ 	@Test
+ 	public void getUserByIdTest(){
+ 		utilisateurDAOtest.getUserById(1);
+ 	}
 }
 
 
